917129d6da26094a0189e2cdfea20e40f009a6ee
Tarocch1 <huiben.fang@gmail.com>
2020-07-30 14:23:45 +0800

    重写 index.d.ts，导出 ObserveOptions

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,17 @@
-declare namespace observer {
-  interface Options {
-    isShallow?: boolean;
-    equals?: (a: unknown, b: unknown) => boolean;
-    ignoreSymbols?: boolean;
-    ignoreUnderscores?: boolean;
-    ignoreKeys?: Array<string | symbol>;
-  }
+export interface ObserveOptions {
+  isShallow?: boolean;
+  equals?: (a: unknown, b: unknown) => boolean;
+  ignoreSymbols?: boolean;
+  ignoreUnderscores?: boolean;
+  ignoreKeys?: Array<string | symbol>;
 }
 
-declare const observer: {
-  observe<T extends { [key: string]: any }>(
-    object: T,
-    callback: (this: T, path: string[], previous: unknown, value: unknown) => unknown,
-    options?: observer.Options,
-  ): T;
-  observed<T extends { [key: string]: any }>(object: T): T;
-  unobserve<T extends { [key: string]: any }>(object: T): T;
-};
+export function observe<T extends { [key: string]: any }>(
+  object: T,
+  callback: (this: T, path: string[], previous: unknown, value: unknown) => unknown,
+  options?: ObserveOptions,
+): T;
 
-export = observer;
+export function observed<T extends { [key: string]: any }>(object: T): T;
+
+export function unobserve<T extends { [key: string]: any }>(object: T): T;
